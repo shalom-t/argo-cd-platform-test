bc42cd6dfd7eaef571b763d686cc0c9bd6c61d26
Shalom Touitou <shalomt@Shaloms-MacBook-Pro.local>
2025-10-08 16:09:43 +0300

    code

---
+++ my-service/my_service/api/v1/api.py
@@ -0,0 +1,8 @@
+# app/api/api_v1/api.py
+from math import log
+from fastapi import APIRouter
+
+from my_service.api.v1.routers import argocd_querier_router
+
+router = APIRouter(prefix="/api/v1")
+router.include_router(argocd_querier_router.router)
